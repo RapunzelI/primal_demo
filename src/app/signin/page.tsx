c18cc6fc34fb1634add6c4f7632e3b310a6f7fed
lainet1 <Lainettiiii@gmail.com>
2025-09-21 18:10:41 +0700

    test commit

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,13 @@
+// pages/index.tsx
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center h-[500px] bg-[#5980DB] space-y-4">
+      <h1>Welcome to signin</h1>
+      <p>This is your starting point.</p>
+    </div>
+  );
+};
+
+export default Home;
